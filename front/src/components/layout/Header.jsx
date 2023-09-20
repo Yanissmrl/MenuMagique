b23eab70cd3c958dfeb062fccc99503d15d917f1
TristeTempsChang <tristan.tran2111@gmail.com>
2023-09-20 17:08:20 +0200

    footer

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
                         <img src={logo} alt="logo" />
                     </div>
                     <div className="nav-text ">
-                        <p className="text-3xl font-bold underline"><FontAwesomeIcon icon={faMagnifyingGlass} className="icon" /> Chercher une recette</p>
+                        <p className="underline"><FontAwesomeIcon icon={faMagnifyingGlass} className="icon" /> Chercher une recette</p>
                         <p><FontAwesomeIcon icon={faCirclePlus} className="icon" /> Ajouter une recette</p>
                         <p><FontAwesomeIcon icon={faUser} className="icon" /> Se connecter</p>
                         <ClassicButton>Crée un compte</ClassicButton>
