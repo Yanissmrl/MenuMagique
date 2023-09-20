01dbca9a1d711309ae835e0c33f527e068939d95
Yaniss Marelle <yanssmarelle@gmail.com>
2023-09-20 12:29:39 +0200

    ingredient list

--- front/src/App.js
+++ front/src/App.js
@@ -2,18 +2,19 @@ import './styles/App.css';
 import Layout from './components/layout/Layout.jsx';
 // import ErrorPage from './components/ErrorPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import FindRecipe from './routes/FindRecipe';
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Layout />,
       // errorElement: <ErrorPage />,
-      // children: [
-      //   {
-      //     path: '/login',
-      //     element: <Login />
-      //   },
-      // ],
+      children: [
+        {
+          path: '/FindRecipe',
+          element: <FindRecipe />
+        },
+      ],
     },
   ]);
 
