d002f5f1b99b020876988f92d8210f85407f3b4e
unMugViolet <jaguinpaul@gmail.com>
2023-09-21 15:40:09 +0200

    feature header color

--- front/src/components/layout/Header.jsx
+++ front/src/components/layout/Header.jsx
@@ -8,35 +8,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
-export default function Header() {
+function Header() {
+  const location = useLocation();
 
-    return (
-        <>
-        <header className="flex justify-center mb-28">
-            <div className="header container">
-                <div className="w-full flex justify-between">
-                    <Link to="/">
-                        <img src={logo} alt="logo" />
-                    </Link>
-                    <div className="flex items-center gap-10">
-                        <Link to="/FindRecipe" className="nav-text">
-                            <p><FontAwesomeIcon icon={faMagnifyingGlass} className="icon" /> Chercher une recette</p>
-                        </Link>
-                        <Link to="/AddRecipe" className="nav-text">
-                            <p><FontAwesomeIcon icon={faCirclePlus} className="icon nav-text" /> Ajouter une recette</p>
-                        </Link>
-                        <Link to="/login" className="nav-text">
-                            <p><FontAwesomeIcon icon={faUser} className="icon nav-text" /> Se connecter</p>
-                        </Link>
+  return (
+    <header className="flex justify-center mb-28">
+      <div className="header container">
+        <div className="w-full flex justify-between">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <div className="flex items-center gap-10">
+            <Link to="/FindRecipe" className={`nav-text ${location.pathname === '/FindRecipe' ? 'color-main-orange ' : ''}`}>
+              <p><FontAwesomeIcon icon={faMagnifyingGlass} className="icon" /> Chercher une recette</p>
+            </Link>
+            <Link to="/AddRecipe" className={`nav-text ${location.pathname === '/AddRecipe' ? 'color-main-orange ' : ''}`}>
+              <p><FontAwesomeIcon icon={faCirclePlus} className="icon nav-text" /> Ajouter une recette</p>
+            </Link>
+            <Link to="/login" className={`nav-text ${location.pathname === '/login' ? 'color-main-orange' : ''}`}>
+              <p><FontAwesomeIcon icon={faUser} className="icon nav-text" /> Se connecter</p>
+            </Link>
 
-                        <ClassicButton to="/register">Crée un compte</ClassicButton>
-                    </div>
-                </div>
-            </div>
-        </header>
-
-        </>
-    )
+            <ClassicButton to="/register">Crée un compte</ClassicButton>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
+
+export default Header;
+
